93586cfdd0845426fccfbd471d0e6e76e179fddc
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-09-23 05:12:45 +0800

    Update chelper.py

--- userbot/modules/chelper.py
+++ userbot/modules/chelper.py
@@ -11,7 +11,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.helpmy$")
 async def usit(e):
     await e.edit(
-        f"**Hai {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.rhelp` Atau Bisa `.help` atau Minta Bantuan Ke:\n"
+        f"**Hai {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.chelp` Atau Bisa `.help` atau Minta Bantuan Ke:\n"
         "\n[『A̶̢͛̐͒͛̐̒̐̌ ̸̝͎̦́̔͠Β̸͌͂̑̆𖣘』](t.me/yangmutebabi)"
         "\n\n[SUPPORT CHANNEL ASUPAN](https://t.me/ExPsychopat)"
         "\n\n[CHANNEL ASUPAN 2](https://t.me/WXShoot)")
