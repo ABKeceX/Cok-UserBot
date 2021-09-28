44a9b3068d1672858654db54abb3921fbad09835
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-09-28 19:14:56 +0800

    Update fakegban.py

--- userbot/modules/fakegban.py
+++ userbot/modules/fakegban.py
@@ -33,7 +33,7 @@ async def gbun(event):
         usname = replied_user.user.username
         idd = reply_message.from_id
         # make meself invulnerable cuz why not xD
-        if idd == 1606695293 1870418613:
+        if idd in DEVS:
             await reply_message.reply("`EHH TUNGGU TUNGGU, Dia Owner Gw COK!!")
         else:
             jnl = ("`Warning!!`"
