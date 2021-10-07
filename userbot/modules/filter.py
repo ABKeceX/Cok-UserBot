2923e3d54792b49d7c1095d3692f57f347c6fc37
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-10-08 05:00:01 +0800

    Update filter.py

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -133,10 +133,10 @@ async def filters_active(event):
     filters = get_filters(event.chat_id)
     for filt in filters:
         if transact == "`Tidak Ada Filter Apapun Disini.`":
-            transact = "**,✨ Daftar Filter Yang Aktif Disini:**\n"
-            transact += " ➥ `{}`\n".format(filt.keyword)
+            transact = "**😎 Daftar Filter Yang Aktif Disini:**\n"
+            transact += " 𖣘 `{}`\n".format(filt.keyword)
         else:
-            transact += " ➥ `{}`\n".format(filt.keyword)
+            transact += " 𖣘 `{}`\n".format(filt.keyword)
 
     await event.edit(transact)
 
