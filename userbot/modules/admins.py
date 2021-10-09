f78979c4a60e6dee8520e98d78281a00042377ec
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-10-10 04:21:32 +0800

    Update admins.py

--- userbot/modules/admins.py
+++ userbot/modules/admins.py
@@ -29,9 +29,20 @@ from telethon.tl.types import (
     MessageMediaPhoto,
     PeerChat,
 )
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import (
+    ChannelParticipantsAdmins,
+    ChatAdminRights,
+    ChatBannedRights,
+    InputChatPhotoEmpty,
+    MessageEntityMentionName,
+    MessageMediaPhoto,
+)
+from telethon.utils import get_display_name
 
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, DEVS
+from userbot import ALIVE_NAME, BOTLOG, BOTLOG_CHATID, CMD_HELP, DEVS
 from userbot.events import register
+from userbot.utils import _format, edit_delete, edit_or_reply
 
 # =================== CONSTANT ===================
 PP_TOO_SMOL = "`Gambar Terlalu Kecil`"
@@ -70,228 +81,18 @@ UNBAN_RIGHTS = ChatBannedRights(
     send_inline=None,
     embed_links=None,
 )
+logging.basicConfig(
+    format="[%(levelname)s- %(asctime)s]- %(name)s- %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
+)
 
+LOGS = logging.getLogger(__name__)
 MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
 
 UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
 # ================================================
 
-
-@register(outgoing=True, pattern=r"^\.setgpic$")
-async def set_group_photo(gpic):
-    if not gpic.is_group:
-        await gpic.edit("`Mohon Lakukan Perintah Ini Di Grup Cok.`")
-        return
-    replymsg = await gpic.get_reply_message()
-    chat = await gpic.get_chat()
-    admin = chat.admin_rights
-    creator = chat.creator
-    photo = None
-
-    if not admin and not creator:
-        return await gpic.edit(NO_ADMIN)
-
-    if replymsg and replymsg.media:
-        await gpic.edit("`Mengubah Profil Grup`")
-        if isinstance(replymsg.media, MessageMediaPhoto):
-            photo = await gpic.client.download_media(message=replymsg.photo)
-        elif "image" in replymsg.media.document.mime_type.split("/"):
-            photo = await gpic.client.download_file(replymsg.media.document)
-        else:
-            await gpic.edit(INVALID_MEDIA)
-
-    if photo:
-        try:
-            await gpic.client(
-                EditPhotoRequest(gpic.chat_id, await gpic.client.upload_file(photo))
-            )
-            await gpic.edit(CHAT_PP_CHANGED)
-
-        except PhotoCropSizeSmallError:
-            await gpic.edit(PP_TOO_SMOL)
-        except ImageProcessFailedError:
-            await gpic.edit(PP_ERROR)
-
-
-@register(outgoing=True, pattern=r"^\.promote(?: |$)(.*)")
-async def promote(promt):
-    # Get targeted chat
-    chat = await promt.get_chat()
-    # Grab admin status or creator in a chat
-    admin = chat.admin_rights
-    creator = chat.creator
-
-    # If not admin and not creator, also return
-    if not admin and not creator:
-        return await promt.edit(NO_ADMIN)
-
-    new_rights = ChatAdminRights(
-        add_admins=False,
-        invite_users=True,
-        change_info=False,
-        ban_users=True,
-        delete_messages=True,
-        pin_messages=True,
-        manage_call=True,
-    )
-
-    await promt.edit("`Kita tambah admin dulu gays!!`")
-    user, rank = await get_user_from_event(promt)
-    if not rank:
-        rank = "Admin"  # Just in case.
-    if not user:
-        return
-
-    # Try to promote if current user is admin or creator
-    try:
-        await promt.client(EditAdminRequest(promt.chat_id, user.id, new_rights, rank))
-        await promt.edit("`Udah di Promote Jangan Semena mena Ya admin baru!`")
-        await sleep(5)
-        await promt.delete()
-
-    # If Telethon spit BadRequestError, assume
-    # we don't have Promote permission
-    except BadRequestError:
-        return await promt.edit(NO_PERM)
-
-    # Announce to the logging group if we have promoted successfully
-    if BOTLOG:
-        await promt.client.send_message(
-            BOTLOG_CHATID,
-            "#PROMOSI\n"
-            f"PENGGUNA: [{user.first_name}](tg://user?id={user.id})\n"
-            f"GRUP: {promt.chat.title}(`{promt.chat_id}`)",
-        )
-
-
-@register(outgoing=True, pattern=r"^\.demote(?: |$)(.*)")
-async def demote(dmod):
-    # Admin right check
-    chat = await dmod.get_chat()
-    admin = chat.admin_rights
-    creator = chat.creator
-
-    if not admin and not creator:
-        return await dmod.edit(NO_ADMIN)
-
-    # If passing, declare that we're going to demote
-    await dmod.edit("`Otw Hapus Admin dulu ges...`")
-    rank = "Admin"  # dummy rank, lol.
-    user = await get_user_from_event(dmod)
-    user = user[0]
-    if not user:
-        return
-
-    # New rights after demotion
-    newrights = ChatAdminRights(
-        add_admins=None,
-        invite_users=None,
-        change_info=None,
-        ban_users=None,
-        delete_messages=None,
-        pin_messages=None,
-        manage_call=None,
-    )
-    # Edit Admin Permission
-    try:
-        await dmod.client(EditAdminRequest(dmod.chat_id, user.id, newrights, rank))
-
-    # If we catch BadRequestError from Telethon
-    # Assume we don't have permission to demote
-    except BadRequestError:
-        return await dmod.edit(NO_PERM)
-    await dmod.edit("`Admin Berhasil Dilepas! Makanya Jangan semena mena tolol`")
-    await sleep(5)
-    await dmod.delete()
-
-    # Announce to the logging group if we have demoted successfully
-    if BOTLOG:
-        await dmod.client.send_message(
-            BOTLOG_CHATID, 
-            "#MENURUNKAN\n"
-            f"PENGGUNA: [{user.first_name}](tg://user?id={user.id})\n"
-            f"GRUP: {dmod.chat.title}(`{dmod.chat_id}`)",
-        )
-
-
-
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-
-from asyncio import sleep
-from os import remove
-
-from telethon.errors import (
-    BadRequestError,
-    ChatAdminRequiredError,
-    ImageProcessFailedError,
-    PhotoCropSizeSmallError,
-    UserAdminInvalidError,
-)
-from telethon.errors.rpcerrorlist import MessageTooLongError, UserIdInvalidError
-from telethon.tl.functions.channels import (
-    EditAdminRequest,
-    EditBannedRequest,
-    EditPhotoRequest,
-)
-from telethon.tl.functions.messages import UpdatePinnedMessageRequest
-from telethon.tl.types import (
-    ChannelParticipantsAdmins,
-    ChannelParticipantsBots,
-    ChatAdminRights,
-    ChatBannedRights,
-    MessageEntityMentionName,
-    MessageMediaPhoto,
-    PeerChat,
-)
-
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
-from userbot.events import register
-
-# =================== CONSTANT ===================
-PP_TOO_SMOL = "`Gambar Terlalu Kecil`"
-PP_ERROR = "`Gagal Memproses Gambar`"
-NO_ADMIN = "`Maaf Anda Bukan Admin:)`"
-NO_PERM = "`Maaf Anda Tidak Mempunyai Izin!`"
-NO_SQL = "`Berjalan Pada Mode Non-SQL`"
-
-CHAT_PP_CHANGED = "`Berhasil Mengubah Profil Gru anjing`"
-CHAT_PP_ERROR = (
-    "`Ada Masalah Dengan Memperbarui Foto,`"
-    "`Mungkin Karna Anda Bukan Admin,`"
-    "`Atau Tidak Mempunyai Izin.`"
-)
-INVALID_MEDIA = "`Media Tidak Valid`"
-
-BANNED_RIGHTS = ChatBannedRights(
-    until_date=None,
-    view_messages=True,
-    send_messages=True,
-    send_media=True,
-    send_stickers=True,
-    send_gifs=True,
-    send_games=True,
-    send_inline=True,
-    embed_links=True,
-)
-
-UNBAN_RIGHTS = ChatBannedRights(
-    until_date=None,
-    send_messages=None,
-    send_media=None,
-    send_stickers=None,
-    send_gifs=None,
-    send_games=None,
-    send_inline=None,
-    embed_links=None,
-)
-
-MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
-
-UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
-# ================================================
 
 
 @register(outgoing=True, pattern=r"^\.setgpic$")
