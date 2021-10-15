d46baa847c2fc06ce94320eafb4cacfb868dc744
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-10-15 13:09:19 +0800

    Update and rename ramscrapers.py to scraperscok.py

--- userbot/modules/scraperscok.py
+++ userbot/modules/scraperscok.py
@@ -988,9 +988,9 @@ async def paste(pstl):
         )
 
 
-@register(outgoing=True, pattern="^.rbg(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.rbg(?: |$)(.*)")
 async def kbg(remob):
-    """ For .rbg command, Remove Image Background. """
+    """For .rbg command, Remove Image Background."""
     if REM_BG_API_KEY is None:
         await remob.edit(
             "`Error: Remove.BG API key missing! Add it to environment vars or config.env.`"
@@ -1004,25 +1004,26 @@ async def kbg(remob):
         await remob.edit("`Processing..`")
         try:
             if isinstance(
-                    reply_message.media, MessageMediaPhoto
-            ) or "image" in reply_message.media.document.mime_type.split('/'):
+                reply_message.media, MessageMediaPhoto
+            ) or "image" in reply_message.media.document.mime_type.split("/"):
                 downloaded_file_name = await remob.client.download_media(
-                    reply_message, TEMP_DOWNLOAD_DIRECTORY)
+                    reply_message, TEMP_DOWNLOAD_DIRECTORY
+                )
                 await remob.edit("`Removing background from this image..`")
                 output_file_name = await ReTrieveFile(downloaded_file_name)
                 os.remove(downloaded_file_name)
             else:
-                await remob.edit("`How do I remove the background from this ?`"
-                                 )
+                await remob.edit("`Bagaimana cara menghapus latar belakang ini ?`")
         except Exception as e:
             await remob.edit(str(e))
             return
     elif input_str:
         await remob.edit(
-            f"`Removing background from online image hosted at`\n{input_str}")
+            f"`Removing background from online image hosted at`\n{input_str}"
+        )
         output_file_name = await ReTrieveURL(input_str)
     else:
-        await remob.edit("`I need something to remove the background from.`")
+        await remob.edit("`Saya butuh sesuatu untuk menghapus latar belakang.`")
         return
     contentType = output_file_name.headers.get("content-type")
     if "image" in contentType:
@@ -1031,18 +1032,21 @@ async def kbg(remob):
             await remob.client.send_file(
                 remob.chat_id,
                 remove_bg_image,
-                caption="Background removed using remove.bg",
+                caption="Aut: @OcongVer2, ThF: @yangmutebabi",
                 force_document=True,
-                reply_to=message_id)
+                reply_to=message_id,
+            )
             await remob.delete()
     else:
-        await remob.edit("**Error (Invalid API key, I guess ?)**\n`{}`".format(
-            output_file_name.content.decode("UTF-8")))
+        await remob.edit(
+            "**Error (Invalid API key, I guess ?)**\n`{}`".format(
+                output_file_name.content.decode("UTF-8")
+            )
+        )
+
 
 # this method will call the API, and return in the appropriate format
 # with the name provided.
-
-
 async def ReTrieveFile(input_file_name):
     headers = {
         "X-API-Key": REM_BG_API_KEY,
@@ -1050,12 +1054,13 @@ async def ReTrieveFile(input_file_name):
     files = {
         "image_file": (input_file_name, open(input_file_name, "rb")),
     }
-    r = requests.post("https://api.remove.bg/v1.0/removebg",
-                      headers=headers,
-                      files=files,
-                      allow_redirects=True,
-                      stream=True)
-    return r
+    return requests.post(
+        "https://api.remove.bg/v1.0/removebg",
+        headers=headers,
+        files=files,
+        allow_redirects=True,
+        stream=True,
+    )
 
 
 async def ReTrieveURL(input_url):
@@ -1063,12 +1068,13 @@ async def ReTrieveURL(input_url):
         "X-API-Key": REM_BG_API_KEY,
     }
     data = {"image_url": input_url}
-    r = requests.post("https://api.remove.bg/v1.0/removebg",
-                      headers=headers,
-                      data=data,
-                      allow_redirects=True,
-                      stream=True)
-    return r
+    return requests.post(
+        "https://api.remove.bg/v1.0/removebg",
+        headers=headers,
+        data=data,
+        allow_redirects=True,
+        stream=True,
+    )
 
 
 @register(outgoing=True, pattern=r"^.direct(?: |$)([\s\S]*)")
