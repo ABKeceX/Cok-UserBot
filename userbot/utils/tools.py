7915d54f7658c74619932626b8ec85995d6937b4
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-10-29 19:59:59 +0800

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -64,6 +64,27 @@ def human_to_bytes(size: str) -> int:
     return int(float(number) * units[unit])
 
 
+def media_type(message):
+    if message and message.photo:
+        return "Photo"
+    if message and message.audio:
+        return "Audio"
+    if message and message.voice:
+        return "Voice"
+    if message and message.video_note:
+        return "Round Video"
+    if message and message.gif:
+        return "Gif"
+    if message and message.sticker:
+        return "Sticker"
+    if message and message.video:
+        return "Video"
+    if message and message.document:
+        return "Document"
+    return None
+
+
+
 async def is_admin(chat_id, user_id):
     req_jo = await bot(GetParticipantRequest(
         channel=chat_id,
