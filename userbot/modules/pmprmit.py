9dbc980933fefe471579a30205d287c63a6a9bd1
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-11-01 19:17:27 +0800

    Update pmprmit.py

--- userbot/modules/pmprmit.py
+++ userbot/modules/pmprmit.py
@@ -428,7 +428,7 @@ class LOG_CHATS:
 LOG_CHATS_ = LOG_CHATS()
 
 
-@tgbot.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+@register(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def monito_p_m_s(event):
     if BOTLOG_CHATID == -100:
         return
@@ -462,7 +462,7 @@ async def monito_p_m_s(event):
                 LOGS.warn(str(e))
 
 
-@tgbot.on(events.NewMessage(incoming=True, func=lambda e: e.mentioned))
+@register(events.NewMessage(incoming=True, func=lambda e: e.mentioned))
 async def log_tagged_messages(event):
     if BOTLOG_CHATID == -100:
         return
