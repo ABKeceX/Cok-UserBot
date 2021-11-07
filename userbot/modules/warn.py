7dfaf254a500e246f4bde326f714015f9ceb0d5f
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-11-07 17:48:44 +0800

    Update warn.py

--- userbot/modules/warn.py
+++ userbot/modules/warn.py
@@ -1,6 +1,5 @@
 # credit catuserbot
 # 𖣘Recode By @yangmutebabi
-#Masih Bug, belum di fix ( ~ < )
 
 from userbot import ALIVE_NAME, BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
@@ -25,7 +24,6 @@ NO_SQL = "`Berjalan Pada Mode Non-SQL`"
 
 @register(outgoing=True, pattern="^.warn(?: |$)(.*)")
 async def _(event):
-    reply_message = await event.get_reply_message()
     admin = chat.admin_rights
     creator = chat.creator
 
@@ -33,10 +31,9 @@ async def _(event):
         return await event.edit(NO_ADMIN)
 
     warn_reason = event.pattern_match.group(1)
-
     if not warn_reason:
         warn_reason = "No reason"
-
+    reply_message = await event.get_reply_message()
     limit, soft_warn = sql.get_warn_setting(event.chat_id)
     num_warns, reasons = sql.warn_user(
         reply_message.sender_id, event.chat_id, warn_reason
@@ -45,16 +42,16 @@ async def _(event):
         sql.reset_warns(reply_message.sender_id, event.chat_id)
         if soft_warn:
             logger.info("TODO: kick user")
-            resalt = f"{} warnings, [User](tg://user?id={}) Has to be KICKED!".format(
+            resalt = f"{} warnings, [user](tg://user?id={}) Has to be KICKED!".format(
                 limit, reply_message.sender_id
             )
         else:
             logger.info("TODO: ban user")
-            resalt += f"{} warnings, [User](tg://user?id={}) Has to be BANNED!".format(
+            resalt += f"{} warnings, [user](tg://user?id={}) Has to be BANNED!".format(
                 limit, reply_message.sender_id
             )
     else:
-        resalt += f"[User](tg://user?id={}) has {}/{} warnings... Tunggu Sebentar!".format(
+        resalt += f"[user](tg://user?id={}) has {}/{} warnings... Tunggu Sebentar!".format(
             reply_message.sender_id, num_warns, limit
         )
         if warn_reason:
@@ -65,23 +62,20 @@ async def _(event):
 @register(outgoing=True, pattern="^.warns(?: |$)(.*)")
 async def _(event):
     "Untuk mendapatkan warn list"
-    reply_message = await event.get_reply_message()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
         return await event.edit(NO_ADMIN)
 
+    reply_message = await event.get_reply_message()
     if not reply_message:
         return await event.edit("__Reply to user to get his warns.__")
-
     result = sql.get_warns(reply_message.sender_id, event.chat_id)
     if not result or result[0] == 0:
         return await event.edit("Pengguna Belum Pernah di warn Tod!!")
-
     num_warns, reasons = result
     limit, soft_warn = sql.get_warn_setting(event.chat_id)
-
     if not reasons:
         return await event.edit("Pengguna ini memiliki {} / {} Warning, tapi tidak ada alasan atas warn nya.".format(
                 num_warns, limit
@@ -99,13 +93,13 @@ async def _(event):
 
 @register(outgoing=True, pattern="^.rmwarns(?: |$)(.*)")
 async def _(event):
-    reply_message = await event.get_reply_message()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
         return await event.edit(NO_ADMIN)
 
+    reply_message = await event.get_reply_message()
     sql.reset_warns(reply_message.sender_id, event.chat_id)
     await event.edit("__Warnings telah di Restart, Kiww__")
 
