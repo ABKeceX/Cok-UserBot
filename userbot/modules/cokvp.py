ec279749247a8e546d638b6d03bc1faf98dda1db
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-11-07 22:52:55 +0800

    Update cokvp.py

--- userbot/modules/cokvp.py
+++ userbot/modules/cokvp.py
@@ -339,19 +339,19 @@ async def on_end_handler(_, u: Update):
 
 CMD_HELP.update(
     {
-        "cokvplay": "**Plugin : **`cokvplay`
-        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vplay` <judul video/link yt>.
-        \n  •↳ : **untuk memutar video di voice chat group dengan akun kamu.
-        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vend`
-        \n  •↳ : **untuk memberhentikan video yang di putar di voice chat group.
-        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vskip`
-        \n  •↳ : **untuk melewati video yang sedang di putar di voice chat group.
-        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vpause`
-        \n  •↳ : **untuk memberhentikan video yang sedang diputar.
-        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vresume`
-        \n  •↳ : **untuk melanjutkan pemutaran video yang sedang diputar.
-        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vplaylist`
-        \n  •↳ : **untuk menampilkan daftar putar video akan di putar.
-        \n\n  •  **note :** bila video sudah masuk ke antrian tapi tidak ngeplay silahkan ketik `.vend` atau `.vskip`"
+        "cokvplay": "**Plugin : **`cokvplay`\
+        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vplay` <judul video/link yt>.\
+        \n  •↳ : **untuk memutar video di voice chat group dengan akun kamu.\
+        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vend`\
+        \n  •↳ : **untuk memberhentikan video yang di putar di voice chat group.\
+        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vskip`\
+        \n  •↳ : **untuk melewati video yang sedang di putar di voice chat group.\
+        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vpause`\
+        \n  •↳ : **untuk memberhentikan video yang sedang diputar.\
+        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vresume`\
+        \n  •↳ : **untuk melanjutkan pemutaran video yang sedang diputar.\
+        \n\n  •  **𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `.vplaylist`\
+        \n  •↳ : **untuk menampilkan daftar putar video akan di putar.\
+        \n\n  •  **note :** bila video sudah masuk ke antrian tapi tidak ngeplay silahkan ketik `.vend` atau `.vskip`\"
     }
 )
