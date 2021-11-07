6bd56cc091045abf5a05835be5dd86527410505e
MTSQD-DuplicateTeam <89113382+ABKeceX@users.noreply.github.com>
2021-11-07 12:39:40 +0800

    Update gkicssspret.py

--- userbot/modules/gkicssspret.py
+++ userbot/modules/gkicssspret.py
@@ -4,18 +4,18 @@ dont edit credits
 """
 
 
-from telethon.tl.types import (
-    MessageEntityMentionName)
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
+from telethon.tl.types import MessageEntityMentionName
 
-from userbot.events import register
-from userbot import ALIVE_NAME, BOTLOG, BOTLOG_CHATID, CMD_HELP, DEVS
+from userbot import ALIVE_NAME, BOTLOG, BOTLOG_CHATID, DEVS
+from userbot import CMD_HELP, bot
+from userbot.events import cok_cmd
 
 
 async def get_user_from_event(event):
-    args = event.pattern_match.group(1).split(':', 1)
+    args = event.pattern_match.group(1).split(":", 1)
     extra = None
-    if event.reply_to_msg_id and not len(args) == 2:
+    if event.reply_to_msg_id and len(args) != 2:
         previous_message = await event.get_reply_message()
         user_obj = await event.client.get_entity(previous_message.from_id)
         extra = event.pattern_match.group(1)
@@ -26,19 +26,20 @@ async def get_user_from_event(event):
         if user.isnumeric():
             user = int(user)
         if not user:
-            await event.edit(f"`{ALIVE_NAME}`: **Kasih Gua nama pengguna,atau id nya tot!!**")
+            await event.edit(
+                f"`{ALIVE_NAME} Berikan Username,ID, atau Reply Chat GOBLOK!!`"
+            )
             return
         if event.message.entities is not None:
             probable_user_mention_entity = event.message.entities[0]
-            if isinstance(probable_user_mention_entity,
-                          MessageEntityMentionName):
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
                 user_id = probable_user_mention_entity.user_id
                 user_obj = await event.client.get_entity(user_id)
                 return user_obj
         try:
             user_obj = await event.client.get_entity(user)
         except Exception as err:
-            return await event.edit("Sepertinya Ada Kesalahan \n **Kesalahan**\n", str(err))
+            return await event.edit("Failed \n **Error**\n", str(err))
     return user_obj, extra
 
 
@@ -52,66 +53,69 @@ async def get_user_from_id(user, event):
         return None
     return user_obj
 
+
 try:
     from userbot import client2, client3
 except BaseException:
     client2 = client3 = None
 
 
-@register(outgoing=True, pattern=r"^\.gkick(?: |$)(.*)")
+@bot.on(cok_cmd(outgoing=True, pattern=r"gkick(?: |$)(.*)"))
 async def gspide(rk):
     lazy = rk
     sender = await lazy.get_sender()
     me = await lazy.client.get_me()
-    if not sender.id == me.id:
-        rkp = await lazy.reply("`Proses global kick jamet tolol!!!`")
+    if sender.id != me.id:
+        rkp = await lazy.reply("`Memprosessssssss...`")
     else:
-        rkp = await lazy.edit("`Proses Global Kick si ngentot...!`")
+        rkp = await lazy.edit("`Memprosessss...`")
     me = await rk.client.get_me()
-    await rkp.edit(f"`{ALIVE_NAME}:` **Melakukan Aksi Global Kick..**")
+    await rkp.edit(f"`{ALIVE_NAME} Memproses Global Kick Goblok!`")
     my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
-    f"@{me.username}" if me.username else my_mention
     await rk.get_chat()
     a = b = 0
     if rk.is_private:
         user = rk.chat
         reason = rk.pattern_match.group(1)
-    else:
-        rk.chat.title
     try:
         user, reason = await get_user_from_event(rk)
     except BaseException:
         pass
     try:
         if not reason:
-            reason = 'Private'
+            reason = "Private"
     except BaseException:
-        return await rkp.edit(f"`{ALIVE_NAME}`, **Kesalahan! Pengguna tidak dikenal.**")
+
+        return await rkp.edit(f"{ALIVE_NAME}, **Kesalahan! Pengguna tidak dikenal.**")
     if user:
         if user.id == self_user.id:
             return await rkp.edit("**Tidak Bisa Kick Diri Sendiri..（>﹏<）**")
 
         if user.id in DEVS:
-            return await rkp.edit(f"`**ETT NGENTOT‼️**, LU GABISA **GKICK** DIA TOLOL,DIA **OWNER** GUA..!!` ")
+            return await rkp.edit(f"{ALIVE_NAME} **ETT NGENTOT‼️**, LU GABISA **GKICK** DIA TOLOL,DIA **OWNER** GUA..!! ")
         try:
             await rk.client(BlockRequest(user))
             await rk.client(UnblockRequest(user))
         except BaseException:
             pass
-        testrk = [d.entity.id for d in await rk.client.get_dialogs() if (d.is_group or d.is_channel)]
+        testrk = [
+            d.entity.id
+            for d in await rk.client.get_dialogs()
+            if (d.is_group or d.is_channel)
+        ]
         for i in testrk:
             try:
                 await rk.client.edit_permissions(i, user, view_messages=False)
                 await rk.client.edit_permissions(i, user, send_messages=True)
                 a += 1
-                await rkp.edit(f"`{ALIVE_NAME} :` **Melakukan Aksi Global Kick dalam {a} obrolan.....**")
+                await rkp.edit(f"{ALIVE_NAME} : **Melakukan Aksi Global Kick dalam {a} obrolan.....**")
 
             except BaseException:
                 b += 1
     else:
-        await rkp.edit(f"`{ALIVE_NAME}:` **Balas ke pengguna !! **")
+        await rkp.edit(f"{ALIVE_NAME}: **Balas ke pengguna !! **")
 
-    return await rkp.edit(f"`{ALIVE_NAME}:` **Melakukan Aksi Global Kick Pada [{user.first_name}](tg://user?id={user.id})\n ID : {user.id} Dalam {a} obrolan(s) **")
+    return await rkp.edit(f"{ALIVE_NAME}: **Melakukan Aksi Global Kick Pada [{user.first_name}](tg://user?id={user.id})\n ID : {user.id} Dalam {a} obrolan(s) **")
 
     if BOTLOG:
         await rkp.client.send_message(
